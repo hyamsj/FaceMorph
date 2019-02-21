b959b69e0f87717965ecb76a8f696125a75f6212
Joni <joni@hyams.ch>
2019-02-21 14:05:42 +0100

    Pictures can no be added individually

--- FaceMorph/MainWindow.xaml.cs
+++ FaceMorph/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,16 +22,53 @@ namespace FaceMorph
     /// </summary>
     public partial class MainWindow : Window
     {
-        int x = 0;
+        public const int IMAGE_WIDTH = 200;
+        List<Image> images = new List<Image>();
+        
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void FileAddFolder_Click(object sender, RoutedEventArgs e)
         {
-            x++;
-            Console.WriteLine($"Button was pressed {x} times");
+            // TODO FolderBrowserDialog
+        }
+
+        private void FileAddImage_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = "";
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filePath = openFileDialog.FileName;
+            }
+            Image image = new Image();
+            ImageSource imageSource = new BitmapImage(new Uri(filePath));
+            image.Source = imageSource;
+            image.Width = IMAGE_WIDTH;
+            image.Margin = new Thickness(10,10,10,10);
+            image.MouseUp += ImageClicked;
+            //image.MouseDown
+
+            
+            images.Add(image);
+            imagePreview.Children.Add(image);
+        }
+
+        private void AddMorePictures_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Add more pictures clicked");                
+        }
+
+        // Probably doesn't belong in this class
+        private void ImageClicked(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Image was clicked");
         }
     }
 }
